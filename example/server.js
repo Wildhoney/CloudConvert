4d0fb9808c00a82971e29dd31b4a030f79a46e67
Adam Timberlake <adam.timberlake@gmail.com>
2013-11-06 00:25:51 +0000

    Updated example for Angular front-end example

--- example/server.js
+++ example/server.js
@@ -19,59 +19,69 @@ app.all('*', function(request, response, next) {
 });
 
 
-var file    = __dirname + '/uploaded-files/Rio.jpg',
-    task    = new CloudConvert().convert(file).from('jpg').into('png');
-
-//task.when('uploading', function(data) {
-//    console.log('Uploading...');
-//});
-
-//task.when('error', function(data) {
-//    console.log(data);
-//});
-
-//task.when('uploaded', function(data) {
-//    console.log('Uploaded...');
-//    console.log(data);
-//});
-
-//task.when('converting', function(data) {
-//    console.log('Converting...');
-//});
-//
-//task.when('finished', function(data) {
-//    console.log('Finished!');
-//    console.log(data);
-//});
-
-task.process();
-
-
 // Responsible for the call to OPTIONS.
 app.options('/', function(request, response) {
     response.send(200);
 });
 
 // Responsible for handling the file upload.
-//app.post('/', function(request, response) {
-//
-//    /**
-//     * @method uploadFile
-//     * @param file {Object}
-//     * @return {void}
-//     */
-//    var uploadFile = function uploadFile(file) {
-//
-//        fileSystem.readFile(file.path, function (error, data) {
-//            var filePath = __dirname + '/uploaded-files/' + file.name;
-//            fileSystem.writeFile(filePath, data, function() {});
-//        });
-//
-//    };
-//
-//    for (var index in request.files) if (request.files.hasOwnProperty(index)) {
-//        var file = request.files[index];
-//        uploadFile(file);
-//    }
-//
-//});
+app.post('/', function(request, response) {
+
+    /**
+     * @method uploadFile
+     * @param file {Object}
+     * @return {void}
+     */
+    var uploadFile = function uploadFile(file) {
+
+        fileSystem.readFile(file.path, function (error, data) {
+
+            var filePath = __dirname + '/uploaded-files/' + file.name;
+            fileSystem.writeFile(filePath, data, function() {});
+            convertFile(filePath);
+
+        });
+
+    };
+
+    /**
+     * @method convertFile
+     * @param filePath {String}
+     * @return {void}
+     */
+    var convertFile = function convertFile(filePath) {
+
+        var task = new CloudConvert().convert(filePath).from('jpg').into('png');
+
+        task.when('uploading', function(data) {
+            console.log('Uploading...');
+        });
+
+        task.when('error', function(data) {
+            console.log(data);
+        });
+
+        task.when('uploaded', function(data) {
+            console.log('Uploaded...');
+            console.log(data);
+        });
+
+        task.when('converting', function(data) {
+            console.log('Converting...');
+        });
+
+        task.when('finished', function(data) {
+            console.log('Finished!');
+            console.log(data);
+        });
+
+        task.process();
+
+    };
+
+    for (var index in request.files) if (request.files.hasOwnProperty(index)) {
+        var file = request.files[index];
+        uploadFile(file);
+    }
+
+});
